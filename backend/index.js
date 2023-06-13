013eb96a9b48016b15b4fed4ac9ed1b3ccf931fb
Shivansh Kumar Dubey <shivanshdubey232@gmail.com>
2023-06-13 11:27:05 +0530

    Update index.js for deployment

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ const connectToMongo = require('./db');
 const app = express()
 const port = 5000;
 const cors = require('cors');
+const path = require('path');
+
 app.use(cors());
 app.use(express.json());
+
 
 connectToMongo();
 
@@ -14,7 +17,11 @@ app.use('/api/notes', require('./routes/notes'));
 app.get('/', (req, res) => {  
   res.send('Hello World!')
 })
-
+//Static files
+app.use(express.static(path.join(__dirname, '/../build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Mindcribe is listening on port ${port}`)
